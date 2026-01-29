40bb796660ea0d9621d2541fc484d872f9ceb1f1
Adam Eivy <atomantic@gmail.com>
2026-01-29 11:05:13 -0800

    remove excess padding

--- client/src/components/layout/Layout.tsx
+++ client/src/components/layout/Layout.tsx
@@ -5,7 +5,7 @@ export function Layout() {
   return (
     <div className="h-screen flex bg-app-bg overflow-hidden">
       <Sidebar />
-      <main className="flex-1 p-6 overflow-y-auto">
+      <main className="flex-1 overflow-y-auto">
         <div className="md:hidden h-14" /> {/* Spacer for mobile hamburger */}
         <Outlet />
       </main>
